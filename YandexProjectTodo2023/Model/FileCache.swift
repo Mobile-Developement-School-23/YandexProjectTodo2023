9bf22775d79b1ca0a2655c8d116dab95c88c089c
TheViLkiS <thevilkis@gmail.com>
2023-06-23 10:42:11 +0400

    homework 1++

---
+++ YandexProjectTodo2023/Model/FileCache.swift
@@ -0,0 +1,168 @@
+import Foundation
+
+enum FileType {
+    case json
+    case csv
+}
+
+enum ErrorToDo: Error {
+    case toDoWithThisIdAlreadyCreated
+}
+
+class FileCache {
+    
+    private var collectionToDo = [String: ToDoItem]()
+    
+    func getCollectionToDo() -> [ToDoItem] {
+        return Array(collectionToDo.values)
+    }
+    
+    func addNewToDo(_ todo: ToDoItem) throws {
+        if collectionToDo[todo.id] == nil {
+            collectionToDo[todo.id] = todo
+        } else {
+            throw ErrorToDo.toDoWithThisIdAlreadyCreated
+        }
+    }
+    
+    func deleteToDo(_ id: String) {
+        collectionToDo[id] = nil
+    }
+    
+    static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
+extension FileCache {
+    
+    private func convertJsonToString() -> String {
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: self.createCache(fileType: .json), options: []) else {
+            print("Convert JSONSerialization error")
+            return ""
+        }
+        let jsonString = String(data: jsonData, encoding: .utf8) ?? ""
+        return jsonString
+    }
+    
+    static func readJsonString(jsonString: String) -> Any {
+        guard let jsonData = jsonString.data(using: String.Encoding.utf8) else {
+            print("couldn't encode string as UTF-8")
+            return ""
+        }
+        guard let dictionaryFunc = try? JSONSerialization.jsonObject(with: jsonData, options: .fragmentsAllowed) else {
+            print("Ошибка FileCahche JSONSerialization")
+            return ""
+        }
+        return dictionaryFunc
+    }
+}
+
+extension FileCache {
+    
+    static func readFromFile(fileName: String, fileType: FileType) -> FileCache? {
+        
+        let fileCache = FileCache()
+        
+        guard fileType == .json else {
+            
+            let fullPath = getDocumentsDirectory().appendingPathComponent(fileName)
+            guard let content = try? String(contentsOf: fullPath) else {
+                print("readFromFile content Error")
+                return nil
+            }
+            let parsedCSV = content.components(separatedBy: "\n").dropLast()
+            for i in parsedCSV {
+                
+                guard let toDoItem = ToDoItem.parseCSV(csv: i) else { continue }
+                do {
+                    try fileCache.addNewToDo(toDoItem)
+                } catch {
+                    return nil
+                }
+            }
+            if fileCache.getCollectionToDo().isEmpty {
+                return nil
+            }
+            return fileCache
+        }
+        
+        let fullPath = getDocumentsDirectory().appendingPathComponent(fileName )
+        guard let content = try? String(contentsOf: fullPath, encoding: .utf8) else {
+            print("readFromFile content Error")
+            return nil
+        }
+        
+        guard let allToDoString = readJsonString(jsonString: content) as? [ String: [Any]] else {
+            print("readFromFile allToDoString Error")
+            return nil
+        }
+        
+        guard let allToDo = allToDoString["collectionToDo"] else {return nil}
+        for iToDo in allToDo {
+            let todoTmp = ToDoItem.parseJson(json: iToDo) ?? ToDoItem(id: "", text: "", priority: .low)
+            
+                try? fileCache.addNewToDo(todoTmp)// ?
+        }
+        return fileCache
+    }
+}
+
+extension FileCache {
+    
+     func createCache(fileType: FileType) -> [String: [Any]] {
+        var todoItemJsonRepresentationArray = [Any]()
+        for item in collectionToDo.values {
+            let todo = fileType == .json ? item.json : item.csv
+            todoItemJsonRepresentationArray.append(todo)
+        }
+        let jsonObject = ["collectionToDo": todoItemJsonRepresentationArray]
+        return jsonObject
+    }
+    
+    func saveToFile(fileName: String, fileType: FileType) {
+        
+        let fullPath = FileCache.getDocumentsDirectory().appendingPathComponent(fileName )
+        var strFrom = String()
+        if fileType == .csv {
+            for i in collectionToDo {
+                
+                let str = (i.value.csv as? String ?? "") + "\n"
+                strFrom.append(str)
+                
+            }
+        } else {
+            strFrom = convertJsonToString()
+        }
+        
+        do {
+            try strFrom.write(to: fullPath, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print( "failed to write file")
+        }
+    }
+}
+// MARK: Save CacheToFileAsync
+
+extension FileCache {
+    
+    static func saveToDefaultFileAsync(collectionToDo: [ToDoItem], collectionToDoComplete: [ToDoItem]) {
+       
+        DispatchQueue.global(qos: .userInitiated).async {
+            
+            let fileCache = FileCache()
+            
+            for i in collectionToDo {
+                if i.creationDate != .distantFuture && i.creationDate != .distantPast {
+                    try? fileCache.addNewToDo(i)
+                }
+            }
+            for i in collectionToDoComplete {
+                try? fileCache.addNewToDo(i)
+            }
+            
+            fileCache.saveToFile(fileName: "fileCacheForTests", fileType: .json)
+        }
+    }
+}
---
+++ YandexProjectTodo2023Tests/YandexProjectTodo2023Tests.swift
@@ -0,0 +1,150 @@
+import XCTest
+@testable import YandexProjectTodo2023
+
+final class YandexToDo2022Tests: XCTestCase {
+    
+    var toDoItem1: ToDoItem?
+    var toDoItem2: ToDoItem?
+    var toDoItem3: ToDoItem?
+    var toDoItem4: ToDoItem?
+    var fileCache: FileCache?
+    var json: Any?
+    var csv: Any?
+    
+    override func setUpWithError() throws {
+        
+        fileCache = FileCache()
+        
+        toDoItem1 = ToDoItem(id: "testID1", text: "testText1", priority: .low, deadline: Date.now.addingTimeInterval(1600), isDone: false, creationDate: .now, modifyDate: Date.now.addingTimeInterval(4200))
+        toDoItem2 = ToDoItem(id: "testID2", text: "testText2", priority: .low, isDone: false, creationDate: .now, modifyDate: Date.now.addingTimeInterval(3200))
+        toDoItem3 = ToDoItem(id: "testID3", text: "testText3", priority: .low, deadline: Date.now.addingTimeInterval(3600), isDone: false, modifyDate: Date.now.addingTimeInterval(2200))
+        toDoItem4 = ToDoItem(id: "testID4", text: "testText4", priority: .low, deadline: Date.now.addingTimeInterval(4600), isDone: false, creationDate: .now)
+        
+        try fileCache?.addNewToDo(toDoItem1!)
+        try fileCache?.addNewToDo(toDoItem2!)
+        try fileCache?.addNewToDo(toDoItem3!)
+        try fileCache?.addNewToDo(toDoItem4!)
+   
+    }
+
+    override func tearDownWithError() throws {
+        toDoItem1 = nil
+        toDoItem2 = nil
+        toDoItem3 = nil
+        toDoItem4 = nil
+        fileCache = nil
+        json = nil
+        csv = nil
+    }
+
+    func testToDoNotNil() {
+        XCTAssertNotNil(toDoItem1)
+    }
+    func testToDoJsonNotNil() {
+        json = toDoItem1?.json
+        XCTAssertNotNil(json)
+    }
+    func testToDoCsvNotNil() {
+        csv = toDoItem1?.csv
+        XCTAssertNotNil(csv)
+    }
+    
+    func testParseJson() {
+        json = toDoItem1?.json
+        let jsonToDo = ToDoItem.parseJson(json: json as Any)!
+      
+        equalTodoItem(todo1: jsonToDo, todo2: toDoItem1!)
+
+    }
+    func testParseCsv() {
+        
+        csv = toDoItem1?.csv
+        let csvToDo = ToDoItem.parseCSV(csv: csv as Any)!
+
+        equalTodoItem(todo1: csvToDo, todo2: toDoItem1!)
+    }
+    
+    func equalTodoItem(todo1: ToDoItem, todo2: ToDoItem) {
+        
+        XCTAssertEqual(todo1.id, todo2.id)
+        XCTAssertEqual(todo1.text, todo2.text)
+        XCTAssertEqual(todo1.priority, todo2.priority)
+        XCTAssertEqual(todo1.deadline?.timeIntervalSince1970, todo2.deadline?.timeIntervalSince1970)
+        XCTAssertEqual(todo1.isDone, todo2.isDone)
+        XCTAssertEqual(todo1.creationDate.timeIntervalSince1970, todo2.creationDate.timeIntervalSince1970)
+        XCTAssertEqual(todo1.modifyDate?.timeIntervalSince1970, todo2.modifyDate?.timeIntervalSince1970)
+
+    }
+    
+    func testSaveLoadCsv() {
+        let collectionTodo = fileCache?.getCollectionToDo()
+        
+        fileCache?.saveToFile(fileName: "testSaveCsv", fileType: .csv)
+
+        let cacheFromFile = FileCache.readFromFile(fileName: "testSaveCsv", fileType: .csv)
+        
+        let collectionTodoFromFile = cacheFromFile?.getCollectionToDo()
+        
+        let countInCollection = collectionTodo?.count == collectionTodoFromFile?.count
+        
+        XCTAssertTrue(countInCollection)
+        
+    }
+    
+    func testSaveLoadJson() {
+        let collectionTodo = fileCache?.getCollectionToDo()
+        
+        fileCache?.saveToFile(fileName: "testSaveJson", fileType: .json)
+        
+        let cacheFromFile = FileCache.readFromFile(fileName: "testSaveJson", fileType: .json)
+        
+        let collectionTodoFromFile = cacheFromFile?.getCollectionToDo()
+        
+        let countInCollection = collectionTodo?.count == collectionTodoFromFile?.count
+
+        XCTAssertTrue(countInCollection)
+    }
+    
+    func testCreateTodoWithOneIdTwice() {
+
+        XCTAssertThrowsError(try fileCache?.addNewToDo(toDoItem1!))
+        XCTAssertThrowsError(try fileCache?.addNewToDo(toDoItem1!))
+    }
+    
+    func testParseCsvWithWrongData() {
+        XCTAssertNil(ToDoItem.parseCSV(csv: toDoItem1 as Any))
+    }
+    func testParseJsonWithWrongData() {
+        XCTAssertNil(ToDoItem.parseJson(json: toDoItem1 as Any))
+    }
+    func testDeleteTodo() {
+        let countBefore = fileCache?.getCollectionToDo().count
+        fileCache?.deleteToDo("testID1")
+        let countAfter = (fileCache?.getCollectionToDo().count)! + 1
+
+        XCTAssertEqual(countBefore, countAfter)
+    }
+    
+    func testReadWrongPath() {
+        XCTAssertNil(FileCache.readFromFile(fileName: "foo", fileType: .csv))
+        XCTAssertNil(FileCache.readFromFile(fileName: "foo", fileType: .json))
+    }
+    
+    func testReadFromWrongData() {
+        
+        let fileName = "foo"
+        let fullPath = FileCache.getDocumentsDirectory().appendingPathComponent(fileName )
+        let strFrom = "efdwrged, sdv sd d \n fgfsd, sdf adf \n sdf, sfsdf  \n"
+        
+        do {
+            try strFrom.write(to: fullPath, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print( "failed to write file")
+        }
+                
+        XCTAssertNil(FileCache.readFromFile(fileName: "foo", fileType: .csv))
+        XCTAssertNil(FileCache.readFromFile(fileName: "foo", fileType: .json))
+
+    }
+    
+}
