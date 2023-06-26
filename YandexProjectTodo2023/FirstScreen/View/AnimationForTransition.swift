5afb8a6967b40df7dcf27b62135d0435725207bf
TheViLkiS <thevilkis@gmail.com>
2023-06-26 10:30:23 +0400

    little fix animation transition

--- YandexProjectTodo2023/FirstScreen/View/AnimationForTransition.swift
+++ YandexProjectTodo2023/FirstScreen/View/AnimationForTransition.swift
@@ -18,7 +18,7 @@ class MyAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         let containerView = transitionContext.containerView
-
+        
         if isPresenting {
             guard let toView = transitionContext.view(forKey: .to) else {return}
             containerView.addSubview(toView)
@@ -31,29 +31,32 @@ class MyAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
                            initialSpringVelocity: 0.8,
                            options: .curveEaseInOut,
                            animations: {
-                                toView.frame = containerView.bounds
-                                toView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-                           }) { _ in
-                                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-                           }
-
+                toView.frame = containerView.bounds
+                toView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }) { _ in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            }
+            
         } else {
             guard let fromView = transitionContext.view(forKey: .from) else {return}
             fromView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            
             UIView.animate(withDuration: transitionDuration(using: transitionContext),
                            delay: 0,
                            usingSpringWithDamping: 0.9,
                            initialSpringVelocity: 0.8,
                            options: .curveEaseIn,
                            animations: {
-                                fromView.frame = self.originFrame
-                                fromView.alpha = 0.1
-                                fromView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-                           }) { _ in
-                                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-                                fromView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-                           }
-
+                fromView.frame = self.originFrame
+                fromView.alpha = 0.1
+                fromView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                fromView.layoutIfNeeded()
+                
+            }) { _ in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+                fromView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }
+            
         }
         
     }
