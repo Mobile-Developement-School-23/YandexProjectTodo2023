d2b5f32e4b26828aecf71e01fa798c1fd2b95717
TheViLkiS <thevilkis@gmail.com>
2023-07-18 14:21:10 +0400

    add down button

--- YandexProjectTodo2023/SwiftUI/MockUI/FirstScreenSwiftUIView.swift
+++ YandexProjectTodo2023/SwiftUI/MockUI/FirstScreenSwiftUIView.swift
@@ -5,37 +5,39 @@ struct FirstScreenSwiftUIView: View {
     @State private var showModal = false
     
     var body: some View {
-        NavigationView {
-            List {
-                HStack {
-                    Image(systemName: "circle").foregroundColor(.gray)
-                    Text("Hello")
-                    Spacer()
-                    Image(systemName: "chevron.right")
-                }.swipeActions(edge: .leading, allowsFullSwipe: true) {
-                    Button {
-                        print("done action")
-                    } label: {
-                        Image(systemName: "checkmark.circle.fill")
+        ZStack {
+            
+            NavigationView {
+                List {
+                    HStack {
+                        Image(systemName: "circle").foregroundColor(.gray)
+                        Text("Hello")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }.swipeActions(edge: .leading, allowsFullSwipe: true) {
+                        Button {
+                            print("done action")
+                        } label: {
+                            Image(systemName: "checkmark.circle.fill")
+                        }
+                        .tint(.green)
+                    }.swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                        Button {
+                            print("trash action")
+                        } label: {
+                            Image(systemName: "trash")
+                        }
+                        .tint(.red)
+                    }.swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button {
+                            print("info action")
+                            self.showModal.toggle()
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                        }
+                        .tint(.gray)
                     }
-                    .tint(.green)
-                }.swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                    Button {
-                        print("trash action")
-                    } label: {
-                        Image(systemName: "trash")
-                    }
-                    .tint(.red)
-                }.swipeActions(edge: .trailing, allowsFullSwipe: false) {
-                    Button {
-                        print("info action")
-                        self.showModal.toggle()
-                    } label: {
-                        Image(systemName: "info.circle.fill")
-                    }
-                    .tint(.gray)
-                }
-                
+                    
                     HStack {
                         Image(systemName: "circle").foregroundColor(.red)
                         Image(systemName: "exclamationmark.2").foregroundColor(.red)
@@ -72,53 +74,72 @@ struct FirstScreenSwiftUIView: View {
                         }
                         .tint(.gray)
                     }
+                    
+                    HStack {
+                        Image(systemName: "circle").foregroundColor(.gray)
+                        Image(systemName: "arrow.down").foregroundColor(.gray)
+                        VStack {
+                            HStack {
+                                Text("Helfsdfcdcfs").lineLimit(3)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "calendar").foregroundColor(.gray).font(.system(size: 14))
+                                Text("18 июля").foregroundColor(.gray).font(.system(size: 14))
+                                Spacer()
+                            }
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }.swipeActions(edge: .leading, allowsFullSwipe: true) {
+                        Button {
+                            print("done action")
+                        } label: {
+                            Image(systemName: "checkmark.circle.fill")
+                        }
+                        .tint(.green)
+                    }.swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                        Button {
+                            print("trash action")
+                        } label: {
+                            Image(systemName: "trash")
+                        }
+                        .tint(.red)
+                    }.swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button {
+                            print("info action")
+                            self.showModal.toggle()
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                        }
+                        .tint(.gray)
+                    }
+                    
+                }
+                .sheet(isPresented: $showModal) {
+                    SecondScreenSwiftUIView()
+                }
+                .navigationTitle(Text("Мои дела"))
                 
-                HStack {
-                    Image(systemName: "circle").foregroundColor(.gray)
-                    Image(systemName: "arrow.down").foregroundColor(.gray)
-                    VStack {
-                        HStack {
-                            Text("Helfsdfcdcfs").lineLimit(3)
-                            Spacer()
-                        }
-                        HStack {
-                            Image(systemName: "calendar").foregroundColor(.gray).font(.system(size: 14))
-                            Text("18 июля").foregroundColor(.gray).font(.system(size: 14))
-                            Spacer()
-                        }
-                    }
-                    Spacer()
-                    Image(systemName: "chevron.right")
-                }.swipeActions(edge: .leading, allowsFullSwipe: true) {
-                    Button {
-                        print("done action")
-                    } label: {
-                        Image(systemName: "checkmark.circle.fill")
-                    }
-                    .tint(.green)
-                }.swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                    Button {
-                        print("trash action")
-                    } label: {
-                        Image(systemName: "trash")
-                    }
-                    .tint(.red)
-                }.swipeActions(edge: .trailing, allowsFullSwipe: false) {
-                    Button {
-                        print("info action")
-                        self.showModal.toggle()
-                    } label: {
-                        Image(systemName: "info.circle.fill")
-                    }
-                    .tint(.gray)
+            }
+            
+            Button(action: {
+                print("Button tapped!")
+                self.showModal.toggle()
+            }) {
+                ZStack {
+                    Color.blue
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .cornerRadius(25)
+                    Image(systemName: "plus")
+                        .foregroundColor(.white)
+                        .scaleEffect(2)
                 }
             }
-            .sheet(isPresented: $showModal) {
-                SecondScreenSwiftUIView()
-            }
-            .navigationTitle(Text("Мои дела"))
+            .padding()
+            .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height - 100)
         }
-
     }
         
 }
